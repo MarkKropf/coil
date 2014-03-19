1d40255e10770e152896d8bef9698754507c00f6
Mark Kropf <markkropf@gmail.com>
2014-03-18 17:38:27 -0700

    Initial github commit
    Load seeds from config if no peers are found
    
    Initial github commit

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-coil
-====
+# Coil
+
+## Overview
+
+**Coil**, a node app to coordinate automation via gossipy communication.
+
+### API
+
+#### Version [/version] GET
+This resource represents the coil software version running on the node  
+Response 200 (text/json)  
+
+#### Digest [/digest] GET
+Get the digest from that node  
+Response 200 (text/json)  
+**id**: UUID of peer  
+**maxversionseen**: vector clock value  
+**transports**: { transports where the node can be reached }  
+
+#### Digest [/digest] PUT
+Push a digest to the node  
+Response 200 (text/json)
+
+#### Delta [/delta] GET
+Get the delta from a prior digest  
+Response 200 (text/json)  
+**id**: UUID of peer  
+**maxversionseen**: vector clock value  
+**transports**: { transports where the node can be reached }  
+
+#### Delta [/delta] PUT
+Push a delta from a prior digest  
+Response 200 (text/json)  
+
+#### Message [/message] GET
+Get messages from a node  
+Response 200 (text/json)
+
+**MessageID**: UUID  
+**State**: Sent, Received, Complete, Error  
+**From**: UUID of the target node  
+**To**: UUID of sending node(s)  
+**Status**: returned status  
+**Version**: version of the message, gets bumped at each state change  
+**Time Sent**: UTC time message was sent  
+**Time Received**: UTC time message was received by To:  
+**Time Completed**: UTC time message was completed  
+**Time of Error**: UTC time the message entered the error state  
+**Message**: Contents of the message  
+
+
+#### Message [/message] PUT
+Push messages to a node  
+Response 200 (text/json)
---
+++ coil.js
@@ -0,0 +1,96 @@
+var nconf = require('nconf');
+var util = require('./lib/coil-util.js');
+var crypto = require('./lib/coil-crypto.js');
+var transport = require('./lib/coil-transport.js');
+var socketio = require('socket.io');
+var peer = require('./lib/coil-peer.js');
+var job = require('./lib/coil-job.js');
+
+// Initialization
+util.welcome();
+nconf.argv()
+	 .env()
+	 .file({ file: 'config.json'});
+
+nconf.argv({
+    "n": {
+      alias: 'newidentity',
+      describe: 'Generate a new identiy',
+      demand: false,
+    },
+    "s": {
+    	alias: 'seed',
+    	describe: 'Add a seed node',
+    	demand: false,
+    }
+  });
+
+// First Run - Generate Identity
+if(nconf.get('newidentity')) {
+	var newId = crypto.createUuid();
+	var newKeys = crypto.createKeys();
+	var newPrivateKey = newKeys.private;
+	var newPublicKey = newKeys.public;
+	nconf.set('peerId',newId);
+	nconf.set('state','alive');
+	nconf.set('privateKey', newPrivateKey);
+	nconf.set('publicKey', newPublicKey);
+	if (peer.createPeer(newId,'','',newPublicKey)) {
+		console.log('[coil] Successfully generated new identity');
+	} else {
+		console.log('[coil] Error while generating new identity');
+	}
+} else {
+	if (!(nconf.get('peerId'))) {
+		console.log('[coil] Error: No configuration data found');
+		process.exit(1);
+	}
+}
+
+// Load Available transports
+var traninit = transport.initialize(nconf.get('transport'),peer);
+
+// Coil Argument to provide seeds at execution
+if(nconf.get('s')) {
+	console.log('[coil] Loading new Seeds');
+	if(nconf.get('seed')==true) {
+		console.log('[coil] Error: No seed input provided');
+		process.exit(1);
+	} else {
+		if(!(nconf.get('seeds'))) {
+			nconf.set('seeds',[]);
+		}
+		nconf.get('seeds').push(nconf.get('seed'));
+	}
+}
+// Check to make sure there are seeds now in the config
+if(!nconf.get('seeds')) {
+	console.log('[coil] Error: No seeds found');
+	process.exit(1);
+}
+// Load in seeds from the config and populate peer.db if empty
+if (peer.readDigest().length <=2) {
+	nconf.get('seeds').forEach(function(value, index, array) {
+		peer.createPeer(value.peerId, value.state, value.transport, "0", value.publicKey, new Date().getTime());
+		console.log("[coil] Adding seed peer: " + value.peerId);
+	});
+}
+
+job.oldestPeer.start();
+//var randomKeys = crypto.createKeys();
+//nconf.set('privateKey', randomKeys.privateKey);
+//nconf.set('publicKey', randomKeys.publicKey);
+// console.log(randomKeys.privateKey);
+// console.log(randomKeys.publicKey);
+// nconf.save(function (err) {
+//    if (err) {
+//      console.error(err.message);
+//      return;
+//    }
+//    console.log('Configuration saved successfully.');
+// });
+// Begin Connecting to peers
+//var priv_pem = nconf.get('privateKey');
+//var pub_pem = nconf.get('publicKey');
+//crypto.encryptMessage(pub_pem,"Hello World");
+// Welcome to Coil
